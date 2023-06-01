fe2ad8a3e6d1aee381a166bb4ace924870ed5cb4
thientantran <thientantm@gmail.com>
2023-06-01 11:32:23 +0700

    Layout for register and login pages

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,3 +1,55 @@
+import { Link } from 'react-router-dom'
+
 export default function Register() {
-  return <div>Register</div>
+  return (
+    <div className='bg-orange'>
+      <div className='mx-auto max-w-7xl px-4'>
+        <div className='grid grid-cols-1 py-12 lg:grid-cols-5 lg:py-32 lg:pr-10'>
+          <div className='lg:col-span-2 lg:col-start-4'>
+            <form className='rounded bg-white p-10 shadow-sm'>
+              <div className='text-2xl'>Dang ky</div>
+              <div className='mt-8'>
+                <input
+                  type='email'
+                  name='email'
+                  placeholder='Email'
+                  className='w-full rounded-sm border border-gray-300 p-3 outline-none focus:border-gray-500 focus:shadow-sm'
+                />
+                <div className='mt-1 min-h-[1rem] text-sm text-red-600'>Email khong hop le</div>
+              </div>
+              <div className='mt-3'>
+                <input
+                  type='password'
+                  name='password'
+                  className='w-full rounded-sm border border-gray-300 p-3 outline-none focus:border-gray-500 focus:shadow-sm'
+                  placeholder='Password'
+                />
+                <div className='mt-1 min-h-[1rem] text-sm text-red-600'></div>
+              </div>
+              <div className='mt-3'>
+                <input
+                  type='password'
+                  name='confirm_password'
+                  className='w-full rounded-sm border border-gray-300 p-3 outline-none focus:border-gray-500 focus:shadow-sm'
+                  placeholder='Confirm password'
+                />
+                <div className='mt-1 min-h-[1rem] text-sm text-red-600'></div>
+              </div>
+              <div className='mt-3'>
+                <button className='w-full bg-red-500 px-2 py-4 text-center text-sm uppercase text-white hover:bg-red-600'>
+                  Dang ky
+                </button>
+              </div>
+              <div className='mt-8 flex justify-center'>
+                <span className='text-gray-400'>Ban da co tai khoan? </span>
+                <Link className='ml-1 text-red-400' to='/login'>
+                  Dang nhap
+                </Link>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
