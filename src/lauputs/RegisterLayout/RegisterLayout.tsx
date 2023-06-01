aad508542653eca6dcf758c40014d6322ed82c47
thientantran <thientantm@gmail.com>
2023-06-01 10:55:21 +0700

    Layout for Register Header and Footer

--- src/lauputs/RegisterLayout/RegisterLayout.tsx
+++ src/lauputs/RegisterLayout/RegisterLayout.tsx
@@ -1,11 +1,15 @@
+import Footer from 'src/components/Footer/Footer'
+import RegisterHeader from 'src/components/RegisterHeader/RegisterHeader'
+
 interface Props {
   children?: React.ReactNode
 }
 export default function RegisterLayout({ children }: Props) {
   return (
     <div>
-      RegisterLayout
+      <RegisterHeader />
       {children}
+      <Footer />
     </div>
   )
 }
