983956ba3ee01b5dedecc680a2fb1cd95770a3b0
thientantran <thientantm@gmail.com>
2023-06-23 09:43:22 +0700

    179.2 Logic for pageinate
    fixup

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,27 +1,75 @@
+import classNames from 'classnames'
+
 interface Props {
   page: number
   setPage: React.Dispatch<React.SetStateAction<number>>
   pageSize: number
 }
-
+const RANGE = 2
 export default function Pagination({ page, setPage, pageSize }: Props) {
   const renderPagination = () => {
+    let dotAfter = false
+    let dotBefore = false
+    const renderDotBefore = (index: number) => {
+      if (!dotBefore) {
+        dotBefore = true
+        return (
+          <button key={index} className='mx-2 rounded border bg-white px-3 py-2 shadow-sm'>
+            ...
+          </button>
+        )
+      }
+      return null
+    }
+    const renderDotAfter = (index: number) => {
+      if (!dotAfter) {
+        dotAfter = true
+        return (
+          <button key={index} className='mx-2 rounded border bg-white px-3 py-2 shadow-sm'>
+            ...
+          </button>
+        )
+      }
+      console.log('ZO-2')
+      return null
+    }
     return Array(pageSize)
       .fill(0)
       .map((_, index) => {
         const pageNumber = index + 1
+        if (page <= RANGE * 2 + 1 && pageNumber > page + RANGE && pageNumber < pageSize - RANGE + 1) {
+          return renderDotAfter(index)
+        } else if (page > RANGE * 2 + 1 && page < pageSize - RANGE * 2) {
+          if (pageNumber < page - RANGE && pageNumber > RANGE) {
+            return renderDotBefore(index)
+          } else if (pageNumber > page + RANGE && pageNumber < pageSize - RANGE + 1) {
+            return renderDotAfter(index)
+          }
+        } else if (page >= pageSize - RANGE * 2 && pageNumber > RANGE && pageNumber < page - RANGE) {
+          return renderDotBefore(index)
+        }
         return (
-          <button key={index} className='mx-2 mt-2 cursor-pointer rounded bg-white px-3 py-2 shadow-sm'>
+          <button
+            key={index}
+            className={classNames('mx-2 cursor-pointer rounded border bg-white px-3 py-2 shadow-sm', {
+              'border-cyan-500': pageNumber === page,
+              'border-transparent': pageNumber !== page
+            })}
+            onClick={() => handleClick(pageNumber)}
+          >
             {pageNumber}
           </button>
         )
       })
   }
+  const handleClick = (pageNumber: number) => {
+    setPage(pageNumber)
+  }
   return (
     <div className='mt-6 flex flex-wrap justify-center'>
-      <button className='mx-2 cursor-pointer rounded bg-white px-3 py-2 shadow-sm'>Prev</button>
+      <button className='mx-2 cursor-pointer rounded border bg-white px-3 py-2 shadow-sm'>Prev</button>
       {renderPagination()}
-      <button className='mx-2 cursor-pointer rounded bg-white px-3 py-2 shadow-sm'>Next</button>
+      <button className='mx-2 cursor-pointer rounded border bg-white px-3 py-2 shadow-sm'>Next</button>
     </div>
   )
 }
