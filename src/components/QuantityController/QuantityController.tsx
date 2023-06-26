ba8b77f4f8f6d7a7a5604a650e295c5edbfbfca5
thientantran <thientantm@gmail.com>
2023-06-26 20:05:54 +0700

    192. 1 Create Quantity Component

---
+++ src/components/QuantityController/QuantityController.tsx
@@ -0,0 +1,38 @@
+import InputNumber from '../InputNumber/InputNumber'
+
+export default function QuantityController() {
+  return (
+    <div className='ml-10 flex items-center'>
+      <button className='flex h-8 w-8 items-center justify-center rounded-l-sm border border-gray-300 text-gray-600'>
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          fill='none'
+          viewBox='0 0 24 24'
+          strokeWidth={1.5}
+          stroke='currentColor'
+          className='h-4 w-4'
+        >
+          <path strokeLinecap='round' strokeLinejoin='round' d='M19.5 12h-15' />
+        </svg>
+      </button>
+      <InputNumber
+        value={1}
+        className=''
+        classNameError='hidden'
+        classNameInput='h-8 w-14 border-t border-b border-gray-300 p-1 text-center outline-none'
+      />
+      <button className='flex h-8 w-8 items-center justify-center rounded-r-sm border border-gray-300 text-gray-600'>
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          fill='none'
+          viewBox='0 0 24 24'
+          strokeWidth={1.5}
+          stroke='currentColor'
+          className='h-4 w-4'
+        >
+          <path strokeLinecap='round' strokeLinejoin='round' d='M12 4.5v15m7.5-7.5h-15' />
+        </svg>
+      </button>
+    </div>
+  )
+}
