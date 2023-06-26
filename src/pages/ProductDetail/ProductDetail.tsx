b9d65a8c363176782997f1c73fa7e79ccdb2150a
thientantran <thientantm@gmail.com>
2023-06-26 21:37:01 +0700

    194.1 Call API addtocart

--- src/pages/ProductDetail/ProductDetail.tsx
+++ src/pages/ProductDetail/ProductDetail.tsx
@@ -1,8 +1,9 @@
-import { useQuery } from '@tanstack/react-query'
+import { useMutation, useQuery } from '@tanstack/react-query'
 import DOMPurify from 'dompurify'
 import { useEffect, useMemo, useRef, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import productApi from 'src/apis/product.api'
+import purchaseApi from 'src/apis/purchase.api'
 import ProductRating from 'src/components/ProductRating/ProductRating'
 import QuantityController from 'src/components/QuantityController/QuantityController'
 import { ProductListConfig, Product as ProductType } from 'src/types/product.type'
@@ -37,6 +38,7 @@ export default function ProductDetail() {
     enabled: Boolean(product)
     // chỉ gọi api khi mà có product, ko có thì ko gọi api này
   })
+  const addToCartMutation = useMutation(purchaseApi.addToCart)
   useEffect(() => {
     if (product && product.images.length > 0) {
       setActiveImage(product.images[0])
@@ -84,6 +86,9 @@ export default function ProductDetail() {
 
   const handleBuyCount = (value: number) => {
     setBuyCount(value)
+  }
+  const addToCart = () => {
+    addToCartMutation.mutate({ buy_count: buyCount, product_id: product?._id as string })
   }
   if (!product) return null
   return (
@@ -187,7 +192,10 @@ export default function ProductDetail() {
                 <div className='ml-6 text-sm text-gray-500'>{product.quantity} sản phẩm có sẵn</div>
               </div>
               <div className='mt-8 flex items-center'>
-                <button className='flex h-12 items-center justify-center rounded-sm border border-orange bg-orange/10 px-5 capitalize text-orange shadow-sm hover:bg-orange/5'>
+                <button
+                  onClick={addToCart}
+                  className='flex h-12 items-center justify-center rounded-sm border border-orange bg-orange/10 px-5 capitalize text-orange shadow-sm hover:bg-orange/5'
+                >
                   <svg
                     enableBackground='new 0 0 15 15'
                     viewBox='0 0 15 15'
