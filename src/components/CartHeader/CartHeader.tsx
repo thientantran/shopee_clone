97ba93133be0d20b780531d00003b619c1776dc8
thientantran <thientantm@gmail.com>
2023-06-30 09:07:34 +0700

    202.5 add form from header into cart header

--- src/components/CartHeader/CartHeader.tsx
+++ src/components/CartHeader/CartHeader.tsx
@@ -25,6 +25,32 @@ export default function CartHeader() {
               <div className='mx-4 h-8 w-[1px] bg-orange'></div>
               <div className='capitalize text-orange lg:text-xl'>Gio Hang</div>
             </Link>
+            <form className='col-span-9' onSubmit={onSubmitSearch}>
+              <div className='flex rounded-sm bg-white p-1'>
+                <input
+                  {...register('name')}
+                  placeholder='Free ship'
+                  type='text'
+                  className='outline-non flex-grow border-none bg-transparent px-3 py-2 text-black'
+                />
+                <button className='flex-shrink-0 rounded-sm bg-orange px-6 py-2 hover:opacity-90'>
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    fill='none'
+                    viewBox='0 0 24 24'
+                    strokeWidth={1.5}
+                    stroke='currentColor'
+                    className='h-6 w-6'
+                  >
+                    <path
+                      strokeLinecap='round'
+                      strokeLinejoin='round'
+                      d='M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z'
+                    />
+                  </svg>
+                </button>
+              </div>
+            </form>
           </nav>
         </div>
       </div>
