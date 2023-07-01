46ba838e8b205eb067468d92e84e715b038d5cd1
thientantran <thientantm@gmail.com>
2023-07-01 15:00:54 +0700

    207. Layout Profile Page

--- src/pages/User/Profile.tsx
+++ src/pages/User/Profile.tsx
@@ -1,3 +1,75 @@
+import Input from 'src/components/Input/Input'
+
 export default function Profile() {
-  return <div>Profile</div>
+  return (
+    <div className='pd-10 rounded-sm bg-white px-2 shadow md:px-7 md:pb-20'>
+      <div className='border-b border-b-gray-200 py-6'>
+        <h1 className='text-lg font-medium capitalize text-gray-900'>Ho so cua toi</h1>
+        <div className='mt-1 text-sm text-gray-700'>Quan ly thong tin ho so de bao mat tai khoan</div>
+      </div>
+      <div className='mt-8 flex flex-col-reverse md:flex-row md:items-start'>
+        <form className='mt-6 flex-grow md:mt-0 md:pr-12'>
+          <div className='flex flex-wrap'>
+            <div className='w-[20%] truncate pt-3 text-right capitalize'>Email</div>
+            <div className='w-[80%] pl-5'>
+              <div className='pt-3 text-gray-700'>tantran@gmail.com</div>
+            </div>
+          </div>
+          <div className='mt-6 flex flex-wrap'>
+            <div className='w-[20%] truncate pt-3 text-right capitalize'>Ten</div>
+            <div className='w-[80%] pl-5'>
+              <Input classNameInput='w-full rounded-sm border border-gray-300 px-3 py-2 outline-none focus:border-gray-500 focus:shadow-sm' />
+            </div>
+          </div>
+          <div className='mt-2 flex flex-wrap'>
+            <div className='w-[20%] truncate pt-3 text-right capitalize'>So dien thoai</div>
+            <div className='w-[80%] pl-5'>
+              <Input classNameInput='w-full rounded-sm border border-gray-300 px-3 py-2 outline-none focus:border-gray-500 focus:shadow-sm' />
+            </div>
+          </div>
+          <div className='mt-2 flex flex-wrap'>
+            <div className='w-[20%] truncate pt-3 text-right capitalize'>Dia chi</div>
+            <div className='w-[80%] pl-5'>
+              <Input classNameInput='w-full rounded-sm border border-gray-300 px-3 py-2 outline-none focus:border-gray-500 focus:shadow-sm' />
+            </div>
+          </div>
+          <div className='mt-2 flex flex-wrap'>
+            <div className='w-[20%] truncate pt-3 text-right capitalize'>Ngay sinh</div>
+            <div className='w-[80%] pl-5'>
+              <div className='flex justify-between'>
+                <select className='h-10 w-[32%] rounded-sm border border-black/10 px-3'>
+                  <option disabled>Ngay</option>
+                </select>
+                <select className='h-10 w-[32%] rounded-sm border border-black/10 px-3'>
+                  <option disabled>Thang</option>
+                </select>
+                <select className='h-10 w-[32%] rounded-sm border border-black/10 px-3'>
+                  <option disabled>Nam</option>
+                </select>
+              </div>
+            </div>
+          </div>
+        </form>
+        <div className='flex justify-center md:w-72 md:border-l md:border-l-gray-200'>
+          <div className='flex flex-col items-center'>
+            <div className='my-5 h-24 w-24'>
+              <img
+                src='https://down-vn.img.susercontent.com/file/br-11134226-7qukw-levcx0zgr2n3d2_tn'
+                alt='avatar'
+                className='f-full w-full rounded-full object-cover'
+              />
+            </div>
+            <input type='file' className='hidden' accept='.jpg, .jpeg,.png' />
+            <button className='flex h-10 items-center justify-end rounded-sm border bg-white px-6 text-sm text-gray-600 shadow-sm'>
+              Chon anh
+            </button>
+            <div className='mt-3 text-gray-400'>
+              <div>Dung luong file toi da 1MB</div>
+              div.Dinh dang: .JPEG, .PNG
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
