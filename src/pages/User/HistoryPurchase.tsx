55714b085f573c0c29d98331ded232156d94bd78
thientantran <thientantm@gmail.com>
2023-07-03 01:09:51 +0700

    217.1 - create component and binding type of status

--- src/pages/User/HistoryPurchase.tsx
+++ src/pages/User/HistoryPurchase.tsx
@@ -1,3 +1,42 @@
+import classNames from 'classnames'
+import { Link, createSearchParams } from 'react-router-dom'
+import { purchasesStatus } from 'src/constants/purchase'
+import useQueryParams from 'src/hooks/useQueryParams'
+// tạo array các tab
+const purchaseTabs = [
+  { status: purchasesStatus.all, name: 'Tất cả' },
+  { status: purchasesStatus.waitForConfirmation, name: 'Chờ xác nhận' },
+  { status: purchasesStatus.waitForGetting, name: 'Chờ lấy hàng' },
+  { status: purchasesStatus.inProgress, name: 'Đang giao' },
+  { status: purchasesStatus.delivered, name: 'Đã giao' },
+  { status: purchasesStatus.cancelled, name: 'Đã huỷ' }
+]
 export default function HistoryPurchase() {
-  return <div>HistoryPurchase</div>
+  // Đối với link cho query params thì nên dùng thẻ Link
+  const queryParams: { status?: string } = useQueryParams()
+  const status: number = Number(queryParams.status) || purchasesStatus.all
+  const purchaseTabsLink = purchaseTabs.map((tab) => (
+    <Link
+      key={tab.status}
+      to={{
+        pathname: '/user/history',
+        search: createSearchParams({ status: String(tab.status) }).toString()
+      }}
+      className={classNames('flex flex-1 items-center justify-center border-b-2 bg-white py-4 text-center', {
+        'border-b-orange text-orange': status === tab.status,
+        'border-b-black/10 text-gray-900': status !== tab.status
+      })}
+    >
+      {tab.name}
+    </Link>
+  ))
+  return (
+    <div>
+      <div className='overflow-x-auto'>
+        <div className='min-w-[700px]'>
+          <div className='sticky top-0 flex rounded-t-sm shadow-sm'>{purchaseTabsLink}</div>
+        </div>
+      </div>
+    </div>
+  )
 }
