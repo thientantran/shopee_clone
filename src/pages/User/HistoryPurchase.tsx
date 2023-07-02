f110034f537ac7ce63616c7254e6578592e12f85
thientantran <thientantm@gmail.com>
2023-07-03 01:21:49 +0700

    217.2 - Fetch API

--- src/pages/User/HistoryPurchase.tsx
+++ src/pages/User/HistoryPurchase.tsx
@@ -1,7 +1,10 @@
+import { useQuery } from '@tanstack/react-query'
 import classNames from 'classnames'
 import { Link, createSearchParams } from 'react-router-dom'
+import purchaseApi from 'src/apis/purchase.api'
 import { purchasesStatus } from 'src/constants/purchase'
 import useQueryParams from 'src/hooks/useQueryParams'
+import { PurchaseListStatus } from 'src/types/purchase.type'
 // tạo array các tab
 const purchaseTabs = [
   { status: purchasesStatus.all, name: 'Tất cả' },
@@ -15,6 +18,12 @@ export default function HistoryPurchase() {
   // Đối với link cho query params thì nên dùng thẻ Link
   const queryParams: { status?: string } = useQueryParams()
   const status: number = Number(queryParams.status) || purchasesStatus.all
+  const { data: purchaseInCartData } = useQuery({
+    queryKey: ['purchases', { status }],
+    queryFn: () => purchaseApi.getPurchases({ status: status as PurchaseListStatus })
+  })
+  const purchaseInCart = purchaseInCartData?.data.data
+  console.log(purchaseInCart)
   const purchaseTabsLink = purchaseTabs.map((tab) => (
     <Link
       key={tab.status}
@@ -35,6 +44,7 @@ export default function HistoryPurchase() {
       <div className='overflow-x-auto'>
         <div className='min-w-[700px]'>
           <div className='sticky top-0 flex rounded-t-sm shadow-sm'>{purchaseTabsLink}</div>
+          <div></div>
         </div>
       </div>
     </div>
