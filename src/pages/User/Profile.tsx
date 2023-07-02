7422b5c06d486d4d7be4a57d735e709a4ffdcce5
thientantran <thientantm@gmail.com>
2023-07-02 21:57:32 +0700

    215. use Form context
    fixup

--- src/pages/User/Profile.tsx
+++ src/pages/User/Profile.tsx
@@ -1,7 +1,7 @@
 import { yupResolver } from '@hookform/resolvers/yup'
 import { useMutation, useQuery } from '@tanstack/react-query'
-import { useContext, useEffect, useMemo, useState } from 'react'
-import { Controller, useForm } from 'react-hook-form'
+import { Fragment, useContext, useEffect, useMemo, useState } from 'react'
+import { Controller, FormProvider, useForm, useFormContext } from 'react-hook-form'
 import { toast } from 'react-toastify'
 import userApi from 'src/apis/user.apis'
 import Button from 'src/components/Button/Button'
@@ -14,6 +14,49 @@ import { setProfileToLS } from 'src/utils/auth'
 import { UserSchema, userSchema } from 'src/utils/rules'
 import { getAvatarURL, isAxiosUnprocessableEntityError } from 'src/utils/utils'
 import DateSelect from './component/DateSelect'
+function Info() {
+  // cái form con này lấy được cái arguments từ 1 thằng provider,
+  const {
+    register,
+    control,
+    formState: { errors }
+  } = useFormContext<FormData>()
+  return (
+    <Fragment>
+      <div className='mt-6 flex flex-col flex-wrap sm:flex-row'>
+        <div className='w-[20%] truncate pt-3 text-right capitalize'>Ten</div>
+        <div className='w-[80%] pl-5'>
+          <Input
+            register={register}
+            name='name'
+            placeholder='Ten'
+            errorMessage={errors.name?.message}
+            classNameInput='w-full rounded-sm border border-gray-300 px-3 py-2 outline-none focus:border-gray-500 focus:shadow-sm'
+          />
+        </div>
+      </div>
+      <div className='mt-2 flex flex-col flex-wrap sm:flex-row'>
+        <div className='w-[20%] truncate pt-3 text-right capitalize'>So dien thoai</div>
+        <div className='w-[80%] pl-5'>
+          <Controller
+            control={control}
+            name='phone'
+            render={({ field }) => (
+              <InputNumber
+                classNameInput='w-full rounded-sm border border-gray-300 px-3 py-2 outline-none focus:border-gray-500 focus:shadow-sm'
+                placeholder='So dien thoai'
+                errorMessage={errors.phone?.message}
+                {...field}
+                onChange={field.onChange}
+                // phai de onChange phia sau, do field co generate ra onChange, nen de onChange phia sau de overwrite
+              />
+            )}
+          />
+        </div>
+      </div>
+    </Fragment>
+  )
+}
 // có 2 flows để upload ảnh lên
 // flow 1: sau khi bấm nút chọn ảnh thì ảnh sẽ được đưa lên server lun, rồi server trả về URL cho ảnh, sau đó nhấn submit thì gửi thông tin lên lại server để lưu URL
 // flow 2: chỉ khi bấm lưu mới gửi ảnh lên server, sau đó mới gọi api để tiến hành upload profile: cách này chậm hơn do gọi 2 api nhưng ko bỉ spam ảnh nhiều lên server
@@ -27,16 +70,8 @@ export default function Profile() {
   const previewImage = useMemo(() => {
     return file ? URL.createObjectURL(file) : ''
   }, [file])
-  const { setProfile, profile: profileFromLS } = useContext(AppContext)
-  const {
-    register,
-    control,
-    formState: { errors },
-    handleSubmit,
-    setValue,
-    watch,
-    setError
-  } = useForm<FormData>({
+  const { setProfile } = useContext(AppContext)
+  const methods = useForm<FormData>({
     defaultValues: {
       name: '',
       phone: '',
@@ -46,6 +81,15 @@ export default function Profile() {
     },
     resolver: yupResolver(profileSchema)
   })
+  const {
+    register,
+    control,
+    formState: { errors },
+    handleSubmit,
+    setValue,
+    watch,
+    setError
+  } = methods
   const avatar = watch('avatar')
   // dùng watch để lấy thông tin
   const { data: profileData, refetch } = useQuery({
@@ -106,94 +150,70 @@ export default function Profile() {
         <h1 className='text-lg font-medium capitalize text-gray-900'>Ho so cua toi</h1>
         <div className='mt-1 text-sm text-gray-700'>Quan ly thong tin ho so de bao mat tai khoan</div>
       </div>
-      <form className='mt-8 flex flex-col-reverse md:flex-row md:items-start' onSubmit={onSubmit}>
-        <div className='mt-6 flex-grow md:mt-0 md:pr-12'>
-          <div className='flex flex-wrap'>
-            <div className='w-[20%] truncate pt-3 text-right capitalize'>Email</div>
-            <div className='w-[80%] pl-5'>
-              <div className='pt-3 text-gray-700'>{profile?.email}</div>
+      <FormProvider {...methods}>
+        <form className='mt-8 flex flex-col-reverse md:flex-row md:items-start' onSubmit={onSubmit}>
+          <div className='mt-6 flex-grow md:mt-0 md:pr-12'>
+            <div className='flex flex-wrap'>
+              <div className='w-[20%] truncate pt-3 text-right capitalize'>Email</div>
+              <div className='w-[80%] pl-5'>
+                <div className='pt-3 text-gray-700'>{profile?.email}</div>
+              </div>
             </div>
-          </div>
-          <div className='mt-6 flex flex-col flex-wrap sm:flex-row'>
-            <div className='w-[20%] truncate pt-3 text-right capitalize'>Ten</div>
-            <div className='w-[80%] pl-5'>
-              <Input
-                register={register}
-                name='name'
-                placeholder='Ten'
-                errorMessage={errors.name?.message}
-                classNameInput='w-full rounded-sm border border-gray-300 px-3 py-2 outline-none focus:border-gray-500 focus:shadow-sm'
-              />
+            <Info />
+            <div className='mt-2 flex flex-col flex-wrap sm:flex-row'>
+              <div className='w-[20%] truncate pt-3 text-right capitalize'>Dia chi</div>
+              <div className='w-[80%] pl-5'>
+                <Input
+                  register={register}
+                  name='address'
+                  placeholder='Dia Chi'
+                  errorMessage={errors.address?.message}
+                  classNameInput='w-full rounded-sm border border-gray-300 px-3 py-2 outline-none focus:border-gray-500 focus:shadow-sm'
+                />
+              </div>
             </div>
-          </div>
-          <div className='mt-2 flex flex-col flex-wrap sm:flex-row'>
-            <div className='w-[20%] truncate pt-3 text-right capitalize'>So dien thoai</div>
-            <div className='w-[80%] pl-5'>
-              <Controller
-                control={control}
-                name='phone'
-                render={({ field }) => (
-                  <InputNumber
-                    classNameInput='w-full rounded-sm border border-gray-300 px-3 py-2 outline-none focus:border-gray-500 focus:shadow-sm'
-                    placeholder='So dien thoai'
-                    errorMessage={errors.phone?.message}
-                    {...field}
-                    onChange={field.onChange}
-                    // phai de onChange phia sau, do field co generate ra onChange, nen de onChange phia sau de overwrite
-                  />
-                )}
-              />
-            </div>
-          </div>
-          <div className='mt-2 flex flex-col flex-wrap sm:flex-row'>
-            <div className='w-[20%] truncate pt-3 text-right capitalize'>Dia chi</div>
-            <div className='w-[80%] pl-5'>
-              <Input
-                register={register}
-                name='address'
-                placeholder='Dia Chi'
-                errorMessage={errors.address?.message}
-                classNameInput='w-full rounded-sm border border-gray-300 px-3 py-2 outline-none focus:border-gray-500 focus:shadow-sm'
-              />
-            </div>
-          </div>
-          <Controller
-            control={control}
-            name='date_of_birth'
-            render={({ field }) => (
-              <DateSelect errorMessage={errors.date_of_birth?.message} value={field.value} onChange={field.onChange} />
-            )}
-          />
+            <Controller
+              control={control}
+              name='date_of_birth'
+              render={({ field }) => (
+                <DateSelect
+                  errorMessage={errors.date_of_birth?.message}
+                  value={field.value}
+                  onChange={field.onChange}
+                />
+              )}
+            />
 
-          <div className='mt-2 flex flex-col flex-wrap sm:flex-row'>
-            <div className='w-[20%] truncate pt-3 text-right capitalize' />
-            <div className='sm:w-[80%] sm:pl-5'>
-              <Button
-                type='submit'
-                className='flex h-9 items-center bg-orange px-5 text-center text-sm text-white hover:bg-orange/80'
-              >
-                Luu
-              </Button>
+            <div className='mt-2 flex flex-col flex-wrap sm:flex-row'>
+              <div className='w-[20%] truncate pt-3 text-right capitalize' />
+              <div className='sm:w-[80%] sm:pl-5'>
+                <Button
+                  type='submit'
+                  className='flex h-9 items-center bg-orange px-5 text-center text-sm text-white hover:bg-orange/80'
+                >
+                  Luu
+                </Button>
+              </div>
             </div>
           </div>
-        </div>
-        <div className='flex justify-center md:w-72 md:border-l md:border-l-gray-200'>
-          <div className='flex flex-col items-center'>
-            <div className='my-5 h-24 w-24'>
-              <img
-                src={previewImage || getAvatarURL(profile?.avatar)}
-                alt='avatar'
-                className='f-full w-full rounded-full object-cover'
-              />
-            </div>
-            <InputFile onChange={handleChangeFile} />
-            <div className='mt-3 text-gray-400'>
-              <div>Dung luong file toi da 1MB</div>
-              <div>Dinh dang: .JPEG, .PNG</div>
+          <div className='flex justify-center md:w-72 md:border-l md:border-l-gray-200'>
+            <div className='flex flex-col items-center'>
+              <div className='my-5 h-24 w-24'>
+                <img
+                  src={previewImage || getAvatarURL(profile?.avatar)}
+                  alt='avatar'
+                  className='f-full w-full rounded-full object-cover'
+                />
+              </div>
+              <InputFile onChange={handleChangeFile} />
+              <div className='mt-3 text-gray-400'>
+                <div>Dung luong file toi da 1MB</div>
+                <div>Dinh dang: .JPEG, .PNG</div>
+              </div>
             </div>
           </div>
-        </div>
-      </form>
+        </form>
+      </FormProvider>
     </div>
   )
 }
