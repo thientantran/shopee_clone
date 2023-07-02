fea8a225bb6e6acaf08bde5d7645cd524d12b724
thientantran <thientantm@gmail.com>
2023-07-03 01:33:25 +0700

    217.3 - Binding data from API

--- src/pages/User/HistoryPurchase.tsx
+++ src/pages/User/HistoryPurchase.tsx
@@ -5,6 +5,7 @@ import purchaseApi from 'src/apis/purchase.api'
 import { purchasesStatus } from 'src/constants/purchase'
 import useQueryParams from 'src/hooks/useQueryParams'
 import { PurchaseListStatus } from 'src/types/purchase.type'
+import { formatCurrency, generateNameId } from 'src/utils/utils'
 // tạo array các tab
 const purchaseTabs = [
   { status: purchasesStatus.all, name: 'Tất cả' },
@@ -44,7 +45,38 @@ export default function HistoryPurchase() {
       <div className='overflow-x-auto'>
         <div className='min-w-[700px]'>
           <div className='sticky top-0 flex rounded-t-sm shadow-sm'>{purchaseTabsLink}</div>
-          <div></div>
+          <div>
+            {purchaseInCart?.map((purchase) => (
+              <div key={purchase._id} className='mt-4 rounded-sm border-black/10 bg-white p-6 text-gray-600 shadow-sm'>
+                <Link
+                  to={`/${generateNameId({ name: purchase.product.name, id: purchase.product._id })}`}
+                  className='flex'
+                >
+                  <div className='flex-shrink-0'>
+                    <img src={purchase.product.image} alt={purchase.product.name} className='h-20 w-20 object-cover' />
+                  </div>
+                  <div className='ml-3 flex-grow overflow-hidden'>
+                    <div className='truncate'>{purchase.product.name}</div>
+                    <div className='mt-3'>x{purchase.buy_count}</div>
+                  </div>
+                  <div className='ml-3 flex-shrink-0'>
+                    <span className='truncate text-gray-500 line-through'>
+                      đ{formatCurrency(purchase.product.price_before_discount)}
+                    </span>
+                    <span className='ml-2 truncate text-orange'>đ{formatCurrency(purchase.product.price)}</span>
+                  </div>
+                </Link>
+                <div className='flex justify-end'>
+                  <div>
+                    <span className='text-lg font-semibold underline'>Tổng giá tiền:</span>
+                    <span className='ml-4 text-xl text-orange'>
+                      đ{formatCurrency(purchase.product.price * purchase.buy_count)}
+                    </span>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
